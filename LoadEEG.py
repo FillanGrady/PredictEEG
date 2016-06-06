2a513f067c99571acb3f8296dbea64be1b727fce
FillanGrady <FillanGrady@gmail.com>
2016-06-06 14:52:04 -0500

    ML side implemented

---
+++ LoadEEG.py
@@ -0,0 +1,228 @@
+import mne
+import numpy as np
+
+"""
+Terminology:
+One recording is an EEG reading session, usually about 24 hours in length
+One file is one chunk of a recording; usually 1 hour in length
+There is a small break between the end of one file and the beginning of the next, but these are negligible
+"""
+
+
+class NextSeizure:
+    """
+    This is a class for determining the amount of time until the next seizure
+    It is only intended to be iterated through once, forward.
+    ONLY call __getitem__ when time is higher than the last time you called it
+    """
+    def __init__(self, seizure_information):
+        self.seizure_information = seizure_information
+        self.file_number = -1
+        self.next_seizure_start_time = -1
+        self.next_seizure_end_time = -1
+
+    def load_next_seizure(self):
+        self.file_number += 1
+        try:
+            while self.seizure_information[self.file_number].number_of_seizures == 0:
+                self.file_number += 1
+        except IndexError as e:
+            self.next_seizure_start_time = float("inf")
+            self.next_seizure_end_time = float("inf")
+        self.next_seizure_start_time = self.seizure_information[self.file_number].seizure_start_time
+        self.next_seizure_end_time = self.seizure_information[self.file_number].seizure_end_time
+
+    def time_until_next_seizure(self, time):
+        """
+        :time: integer time
+        :return: time until next seizure
+        """
+        if time < self.next_seizure_start_time:
+            return self.next_seizure_start_time - time
+        if time < self.next_seizure_end_time:
+            return 0
+        else:
+            self.load_next_seizure()
+            return self.time_until_next_seizure(time)
+
+    def time_until_next_seizure_array(self, time_array, number_output_neurons):
+        """
+        :time_array: numpy array of times
+        :return: numpy array of time until the next seizure
+        """
+        shape = list(time_array.shape)
+        shape.append(number_output_neurons)
+        time_until = np.zeros(shape)
+        for indicies, value in np.ndenumerate(time_array):
+            indicies = list(indicies)
+            time_until_next_seizure = int(self.time_until_next_seizure(value) / 60) # In minutes
+            if time_until_next_seizure < number_output_neurons - 1:
+                indicies.append(int(self.time_until_next_seizure(value)))
+                time_until[tuple(indicies)] = 1
+            else:
+                indicies.append(number_output_neurons - 1)
+                time_until[tuple(indicies)] = 1
+        return time_until
+
+
+class NextEEG:
+    """
+    This class is also designed to be called iteratively
+    If you keep on calling next_eeg, it'll iterate through the edf EEG files,
+    and return the EEG readings and the time since the start of the recording
+    """
+    def __init__(self, patient_number, seizure_information):
+        self.patient_number = patient_number
+        self.seizure_information = seizure_information
+        self.file_index = -1
+        self.data = np.zeros((0, 0))
+        self.times = np.zeros((0, 0))
+        self.index = 0
+        self.sampling_frequency = -1  # Sample per second
+        self.load_new_file()
+
+    def next_eeg_samples(self, number_samples):
+        """
+        Returns the EEG readings and time for the next timepoint
+        Use to iterate through data
+        Returns a numpy array and a float
+        """
+        if self.index + number_samples >= self.times.shape[0]:
+            """
+            Gone over the end of the file.  Fill in the missing information with 0s, and switch to a new file
+            """
+            before_file_end = self.data[self.index:, :]
+            after_file_end = np.zeros((self.index + number_samples - self.times.shape[0], self.data.shape[1]))
+            eeg_data = np.concatenate([before_file_end, after_file_end])
+            before_file_end = self.times[self.index:]
+            after_file_end = np.zeros(self.index + number_samples)
+            time_data = np.concatenate([before_file_end, after_file_end])
+
+            self.load_new_file()
+        else:
+            eeg_data = self.data[self.index:self.index + number_samples, :]
+            time_data = self.times[self.index:self.index + number_samples]
+            self.index += number_samples
+        return eeg_data, time_data
+
+    def next_eeg_seconds(self, number_seconds):
+        return self.next_eeg_samples(int(number_seconds * self.sampling_frequency))
+
+    def load_new_file(self):
+        self.file_index += 1
+        if self.file_index > len(self.seizure_information):
+            raise StopIteration
+        try:
+            next_eeg_file_path = 'chb%02d_%02d.edf' % \
+                                 (self.patient_number, self.seizure_information[self.file_index].patient_recording)
+            raw = mne.io.read_raw_edf(next_eeg_file_path)
+        except IOError as e:
+            print e
+            raise IOError("Download missing file")
+        self.data, self.times = raw[:, :]
+        self.times = np.array(self.times)
+        self.data = self.data.T
+        self.sampling_frequency = (self.times.shape[0] - 1) / (self.times[-1] - self.times[0])
+        self.times += self.seizure_information[self.file_index].start_time
+        self.index = 0
+
+
+class EEGInformation:
+    """
+    Used to store information about one EEG file
+    """
+    start_time = None
+    end_time = None
+    seizure_start_time = None
+    seizure_end_time = None
+    number_of_seizures = None
+    patient_number = None
+    patient_recording = None
+    total_seconds = None
+
+    def __init__(self, patient_number, patient_recording, buffer):
+        self.patient_number = patient_number
+        self.patient_recording = patient_recording
+        for line in buffer:
+            if "File Start Time" in line:
+                time = [int(x) for x in line[line.index(": ") + 2:].split(':')]
+                self.start_time = (time[0] * 60 * 60) + (time[1] * 60) + time[2]
+            if "File End Time" in line:
+                time = [int(x) for x in line[line.index(": ") + 2:].split(':')]
+                self.end_time = (time[0] * 60 * 60) + (time[1] * 60) + time[2]
+                if self.end_time < self.start_time:  # Overflowed, new day
+                    self.end_time += 24 * 60 * 60  # Number of seconds in a day
+                self.total_seconds = self.end_time - self.start_time
+            if "Number of Seizures in File" in line:
+                self.number_of_seizures = int("".join([x for x in line if x.isdigit()]))
+                assert self.number_of_seizures <= 1  # Assume that there's only one seizure per file
+                if self.number_of_seizures == 0:
+                    self.seizure_start_time = None
+                    self.seizure_end_time = None
+                    return
+            if "Seizure Start Time" in line:
+                self.seizure_start_time = int("".join([x for x in line if x.isdigit()]))
+            if "Seizure End Time" in line:
+                self.seizure_end_time = int("".join([x for x in line if x.isdigit()]))
+
+    def __str__(self):
+        return "Time: %s --- %s\nNumber of Seizures: %s\nSeizure Time: %s-%s" %\
+               (self.start_time, self.end_time, self.number_of_seizures, self.seizure_start_time, self.seizure_end_time)
+
+
+def get_seizure_information(patient_number):
+    """
+    Returns information about eeg files in summary file
+    """
+    import fnmatch
+    summary_file_path = 'chb%02d-summary.txt' % patient_number
+    eeg_file_path = '*chb%02d_*.edf*' % patient_number
+    found_file = False
+    recording_number = -1
+    buffer = []
+    seizure_information = []
+    with open(summary_file_path) as summary_file:
+        for line in summary_file:
+            if not found_file:
+                if fnmatch.fnmatch(line, eeg_file_path):
+                    found_file = True
+                    recording_number = int(line[line.index("chb%02d_" % patient_number) + 6:line.index(".edf")])
+                    buffer = []
+            else:
+                if line != "\n":
+                    buffer.append(line.strip())
+                else:
+                    seizure_information.append(EEGInformation(patient_number, recording_number, buffer))
+                    found_file = False
+                    recording_number = -1
+
+    print "%s eeg files for patient %s" % (len(seizure_information), patient_number)
+
+    #  Rearranges the times so that they start at time = 0, and to properly account for days passing
+    start_recording = seizure_information[0].start_time
+    for s in seizure_information:
+        s.start_time -= start_recording
+        s.end_time -= start_recording
+    for i in range(len(seizure_information) - 1):
+        while seizure_information[i].start_time > seizure_information[i + 1].start_time:
+            seizure_information[i + 1].start_time += 24 * 60 * 60
+            seizure_information[i + 1].end_time += 24 * 60 * 60
+    for s in seizure_information:
+        if s.seizure_start_time is not None:
+            s.seizure_start_time += s.start_time
+        if s.seizure_end_time is not None:
+            s.seizure_end_time += s.start_time
+
+    return seizure_information
+
+
+def setup(patient_number):
+    seizure_information = get_seizure_information(patient_number)
+    ne = NextEEG(patient_number, seizure_information)
+    ns = NextSeizure(seizure_information)
+    return ne, ns
+
+if __name__ == '__main__':
+    ne, ns = setup(1)
+    eeg_data, time_data = ne.next_eeg_seconds(5)
+    time_until = ns.time_until_next_seizure_array(time_data, 31)
--- PredictEEG.py
+++ PredictEEG.py
@@ -1,177 +1,81 @@
-#import tensorflow as tf
-import mne
+import LoadEEG
+import tensorflow as tf
 import numpy as np
+from tensorflow.models.rnn import rnn, rnn_cell
 
 """
-Terminology:
-One recording is an EEG reading session, usually about 24 hours in length
-One file is one chunk of a recording; usually 1 hour in length
-There is a small break between the end of one file and the beginning of the next, but these are negligible
+Neural Network model
+Input -> Convolution Layer -> Max Pool -> Convolution Layer -> Max Pool -> LTSM Layer -> Connected Layer -> Output
+      256                256*6       128*6                128*6     64*6->432         60                 30
 """
 
-class NextSeizure:
-    """
-    This is a class for determining the amount of time until the next seizure
-    It is only intended to be iterated through once, forward.
-    ONLY call next_seizure when time is higher than the last time you called it
-    """
-    def __init__(self, seizure_information):
-        self.seizure_information = seizure_information
-        self.file_number = -1
-        self.next_seizure_start_time = -1
-        self.next_seizure_end_time = -1
-    
-    def next_seizure(self, time):
-        """
-        Returns time (in seconds) until next seizure
-        """
-        if time < self.next_seizure_start_time:
-            return self.next_seizure_start_time - time
-        elif time < self.next_seizure_end_time:
-            return 0
-        else:
-            self.file_number += 1
-            try:
-                while self.seizure_information[self.file_number].number_of_seizures == 0:
-                    self.file_number += 1
-            except IndexError as e:
-                self.next_seizure_start_time = float("inf")
-                self.next_seizure_end_time = float("inf")
-                return self.next_seizure(time)
-            self.next_seizure_start_time = self.seizure_information[self.file_number].seizure_start_time
-            self.next_seizure_end_time = self.seizure_information[self.file_number].seizure_end_time
-            return self.next_seizure(time)
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape=shape, stddev=0.1)
+    return tf.Variable(initial)
 
 
-class NextEEG:
-    """
-    This class is also designed to be called iteratively
-    If you keep on calling next_eeg, it'll iterate through the edf EEG files,
-    and return the EEG readings and the time since the start of the recording
-    """
-    def __init__(self, patient_number, seizure_information):
-        self.patient_number = patient_number
-        self.seizure_information = seizure_information
-        self.file_index = -1
-        self.data = np.zeros((0, 0))
-        self.times = np.zeros((0, 0))
-        self.index = -1
-
-    def next_eeg(self):
-        """
-        Returns the EEG readings and time for the next timepoint
-        Use to iterate through data
-        Returns a numpy array and a float
-        """
-        self.index += 1
-        if self.index >= self.times.shape[0]:
-            self.file_index += 1
-            if self.file_index > len(self.seizure_information):
-                raise StopIteration
-            try:
-                next_eeg_file_path = 'chb%02d_%02d.edf' % \
-                                 (self.patient_number, self.seizure_information[self.file_index].patient_recording)
-                raw = mne.io.read_raw_edf(next_eeg_file_path)
-            except IOError as e:
-                print e
-                raise IOError("Download missing file")
-            self.data, self.times = raw[:, :]
-            self.times = np.array(self.times)
-            self.data = self.data.T
-            self.times += self.seizure_information[self.file_index].start_time
-            self.index = -1
-            return self.next_eeg()
-        else:
-            return self.data[self.index, :], self.times[self.index]
+def bias_variable(shape):
+    initial = tf.constant(0.1, shape=shape)
+    return tf.Variable(initial)
 
 
-class EEGInformation:
-    """
-    Used to store information about one EEG file
-    """
-    start_time = None
-    end_time = None
-    seizure_start_time = None
-    seizure_end_time = None
-    number_of_seizures = None
-    patient_number = None
-    patient_recording = None
-    total_seconds = None
-
-    def __init__(self, patient_number, patient_recording, buffer):
-        self.patient_number = patient_number
-        self.patient_recording = patient_recording
-        for line in buffer:
-            if "File Start Time" in line:
-                time = [int(x) for x in line[line.index(": ") + 2:].split(':')]
-                self.start_time = (time[0] * 60 * 60) + (time[1] * 60) + time[2]
-            if "File End Time" in line:
-                time = [int(x) for x in line[line.index(": ") + 2:].split(':')]
-                self.end_time = (time[0] * 60 * 60) + (time[1] * 60) + time[2]
-                if self.end_time < self.start_time:  # Overflowed, new day
-                    self.end_time += 24 * 60 * 60  # Number of seconds in a day
-                self.total_seconds = self.end_time - self.start_time
-            if "Number of Seizures in File" in line:
-                self.number_of_seizures = int("".join([x for x in line if x.isdigit()]))
-                assert self.number_of_seizures <= 1  # Assume that there's only one seizure per file
-                if self.number_of_seizures == 0:
-                    self.seizure_start_time = None
-                    self.seizure_end_time = None
-                    return
-            if "Seizure Start Time" in line:
-                self.seizure_start_time = int("".join([x for x in line if x.isdigit()]))
-            if "Seizure End Time" in line:
-                self.seizure_end_time = int("".join([x for x in line if x.isdigit()]))
-
-    def __str__(self):
-        return "Time: %s --- %s\nNumber of Seizures: %s\nSeizure Time: %s-%s" %\
-               (self.start_time, self.end_time, self.number_of_seizures, self.seizure_start_time, self.seizure_end_time)
+def conv1d(x, W):
+    return tf.nn.conv2d(input=x, filter=W, strides=[1, 1, 1, 1], padding='SAME')
 
 
-def get_seizure_information(patient_number, patient_recording):
-    """
-    Returns information about seizure file"""
-    summary_file_path = 'chb%02d-summary.txt' % patient_number
-    eeg_file_path = 'chb%02d_%02d.edf' % (patient_number, patient_recording)
-    found_file = False
-    buffer = []
-    with open(summary_file_path) as summary_file:
-        for line in summary_file:
-            if not found_file:
-                if eeg_file_path in line:
-                    found_file = True
-            else:
-                if line != "\n":
-                    buffer.append(line.strip())
-                else:
-                    return EEGInformation(patient_number, patient_recording, buffer)
-    return None
+def max_pool_2(x):
+    return tf.nn.max_pool(x, ksize=[1, 2, 1, 1], strides=[1, 2, 1, 1], padding='SAME')
 
 if __name__ == '__main__':
-    seizure_information = []
-    number_files = 46
-    for i in range(1, number_files+1):
-        s = get_seizure_information(1, i)
-        if s is not None:
-            seizure_information.append(s)
-    start_recording = seizure_information[0].start_time
-    for s in seizure_information:
-        s.start_time -= start_recording
-        s.end_time -= start_recording
-    for i in range(len(seizure_information) - 1):
-        while seizure_information[i].start_time > seizure_information[i + 1].start_time:
-            seizure_information[i + 1].start_time += 24 * 60 * 60
-            seizure_information[i + 1].end_time += 24 * 60 * 60
-    for s in seizure_information:
-        if s.seizure_start_time is not None:
-            s.seizure_start_time += s.start_time
-        if s.seizure_end_time is not None:
-            s.seizure_end_time += s.start_time
-    ns = NextSeizure(seizure_information)
-    ne = NextEEG(1, seizure_information)
-    try:
-        while True:
-            data, time = ne.next_eeg()
-            time_to_next_seizure = ns.next_seizure(time)
-    except StopIteration:
-        print "Done!"
+    ne, ns = LoadEEG.setup(1)
+    learning_rate = 0.001
+    training_iters = 10
+    display_step = 1
+
+    n_samples_per_second = 256
+    n_lstm_input = 12
+    n_lstm_output = 384
+    n_output = 31
+    input_depth = 23
+
+    x = tf.placeholder(tf.float32, [None, input_depth])
+    y = tf.placeholder(tf.float32, [None, n_output])
+
+    depths = {'conv1': 12, 'conv2': 12}
+    weights = {'conv1': weight_variable([10, 1, input_depth, depths['conv1']]),
+               'conv2': weight_variable([10, 1, depths['conv2'], depths['conv1']]),
+               'full': weight_variable([n_lstm_output, n_output])}
+    biases = {'conv1': bias_variable([depths['conv1']]),
+              'conv2': bias_variable([depths['conv2']]),
+              'full': bias_variable([n_output])}
+    input = tf.reshape(x, [1, -1, 1, input_depth])
+    h_conv1 = tf.nn.relu(conv1d(input, weights['conv1']) + biases['conv1'])
+    h_pool1 = max_pool_2(h_conv1)
+
+    h_conv2 = tf.nn.relu(conv1d(h_pool1, weights['conv2']) + biases['conv2'])
+    h_pool2 = max_pool_2(h_conv2)
+
+    flat = tf.reshape(h_pool2, [-1, n_lstm_input])
+    s = tf.split(0, 64, flat)
+
+    lstm = rnn_cell.BasicLSTMCell(num_units=n_lstm_output, forget_bias=1.0)
+    outputs, states = rnn.rnn(lstm, s, dtype=tf.float32)
+    prediction = tf.nn.softmax(tf.matmul(outputs[-1], weights['full']) + biases['full'])
+
+    correct_prediction = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
+    accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+    cost = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(prediction, y))
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(cost)
+
+    init = tf.initialize_all_variables()
+    with tf.Session() as sess:
+        sess.run(init)
+        for i in range(training_iters):
+            eeg_data, time_data = ne.next_eeg_seconds(5)
+            time_until = ns.time_until_next_seizure_array(time_data, n_output)
+            sess.run(optimizer, feed_dict={x: eeg_data, y: time_until})
+            if i % display_step == 0:
+                acc = sess.run(accuracy, feed_dict={x: eeg_data, y: time_until})
+                print "Iter: %.0f, Accuracy: %.4f" % (i, acc)
